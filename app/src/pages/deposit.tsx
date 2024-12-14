5f0e7416b9de975a974fd602435e4d69fb2746c1
sven-hash <something@local>
2024-12-14 13:47:45 +0100

    minor changes

--- app/src/pages/deposit.tsx
+++ app/src/pages/deposit.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import { DepositPage } from '@/components/Deposit'
+import { ConvertPage } from '@/components/ConvertPage'
 
 export default function Deposit() {
   return (
@@ -11,7 +11,7 @@ export default function Deposit() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <DepositPage />
+      <ConvertPage />
     </>
   )
 } 
