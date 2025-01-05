19324c3dff2ef442b9649fe46e84b7e9f4eb9ea6
sven-hash <>
2025-01-05 11:05:21 +0100

    minor changes

--- appv2/next.config.js
+++ appv2/next.config.js
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/*const nextConfig = {
   images: {
     domains: [
       'raw.githubusercontent.com',
     ],
   },
+}
+
+module.exports = nextConfig*/
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    domains: [
+      'raw.githubusercontent.com',
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+    }
+    config.externals.push('pino-pretty', 'encoding')
+    return config
+  }
 }
 
 module.exports = nextConfig
